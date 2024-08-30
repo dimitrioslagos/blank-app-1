119d10688cbea09fa9b6fd4d5fef2772fa27496e
dimitrioslagos <lagosdemetres@gmail.com>
2024-08-30 14:38:22 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 # Read the HTML file
-with open("example.html", "r") as file:
+with open("pandapower_network_map.html", "r") as file:
     html_content = file.read()
 
 # Display the HTML in Streamlit
