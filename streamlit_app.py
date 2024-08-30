f98303af31f91066764dc307b2d845669dbf501e
dimitrioslagos <lagosdemetres@gmail.com>
2024-08-30 14:41:42 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,16 @@ import streamlit as st
 import streamlit as st
 import streamlit.components.v1 as components
 
+# Create a two-column layout
+col1, col2 = st.columns(2)
+
 # Read the HTML file
 with open("pandapower_network_map.html", "r") as file:
     html_content = file.read()
 
-# Display the HTML in Streamlit
-components.html(html_content, height=500)  # You can adjust the height as needed
+
+# Display HTML content in the left column
+with col1:
+    st.subheader("Load Flow Results")
+    components.html(html_content, height=300, scrolling=True)
+
